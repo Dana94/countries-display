b09bbd92088fe8d52b098c10df3f6dd33d1eaca1
Dana <dana.ottaviani@gmail.com>
2020-09-12 10:39:21 -0400

    WIP

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,7 +7,12 @@ const Card = (props) => {
         <div className={classes.Card}>
             <img src={props.country.flag.svgFile} alt={`${props.country.name}'s Flag`} />
             <div className={classes.Content}>
-                {props.country.name}
+                <p>{props.country.name}</p>
+                <ul>
+                    {
+                        props.country.currencies.map(currency => <li key={currency._id}>{currency.name}</li>)
+                    }
+                </ul>
             </div>
         </div>
     )
