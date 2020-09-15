b9111ec409290de6156230c6478d7b8cd07a37bf
Dana <dana.ottaviani@gmail.com>
2020-09-15 10:23:55 -0400

    fix: only include first 2 currencies
    some countries have at least 5, and filter out "null" value

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,12 +8,15 @@ const Card = (props) => {
             <img src={props.country.flag.svgFile} alt={`${props.country.name}'s Flag`} />
             <div className={classes.Content}>
                 <p>{props.country.name}</p>
-                <ul>
-                    {
-                        props.country.currencies.map(currency => <li key={currency._id}>{currency.name}</li>)
-                    }
-                </ul>
             </div>
+            <ul>
+                {
+                    props.country.currencies
+                        .filter(currency => currency.name !== "null")
+                        .slice(0, 2) // only give first 2 currencies
+                        .map(currency => <li key={currency._id}>{currency.name}</li>)
+                }
+            </ul>
         </div>
     )
 }
