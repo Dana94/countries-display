e6c52cfd93c556ce59efdc93b529387f42fb78a0
Dana <dana.ottaviani@gmail.com>
2020-09-20 10:57:56 -0400

    feat: display languages in scroll format

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,7 +4,33 @@ import classes from './Menu.module.css';
 
 const Menu = props => {
     return (
-        <div className={classes.Menu}>Menu</div>
+        <div className={classes.Menu}>
+            <div className={classes.Filters}>
+                Menu Filters
+                <hr />
+                <fieldset>
+                    <legend>Languages</legend>
+                    <div className={classes.Langs}>
+                        {
+                            props.languages.map(lang => {
+                                return (
+                                    <div key={lang._id} className={classes.Lang}>
+                                        <input id={lang._id} type="checkbox" name={lang.name} value={lang._id} />
+                                        <label for={lang._id}>{lang.name}</label>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </fieldset>
+                <div>
+                </div>
+                <hr />
+                Population
+                <hr />
+                Currency
+            </div>
+        </div>
     )
 }
 
