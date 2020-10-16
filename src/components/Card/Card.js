336b3bce3bacd57a2d3d80c1879828baa15c714c
Dana <dana.ottaviani@gmail.com>
2020-10-16 10:29:05 -0400

    fix: lists in smaller mobile

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -10,19 +10,6 @@ const Card = (props) => {
             <div className={classes.Content}>
                 <p>{props.country.name}</p>
             </div>
-            {/* <ul className={classes.List}>
-                    {
-                        props.country.officialLanguages.slice(0, 2).map(lang => <li key={lang.name}>{lang.name}</li>)
-                    }
-                </ul>
-                <ul className={classes.List}>
-                    {
-                        props.country.currencies
-                            .filter(currency => currency.name !== "null")
-                            .slice(0, 2) // only give first 2 currencies
-                            .map(currency => <li key={currency._id}>{currency.name}</li>)
-                    }
-                </ul> */}
         </div>
     )
 }
