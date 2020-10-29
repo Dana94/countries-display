1d7f4bfd558ef58598c6b2957007e31146403ccf
Dana <dana.ottaviani@gmail.com>
2020-10-29 10:41:00 -0400

    feat: show focus/hover affect on Cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-import classes from './Card.module.css'
+import './Card.css'
 
 const Card = (props) => {
 
+    const _onFocus = () => {
+        console.log('on focus')
+    }
+
+    const _onClick = () => {
+        console.log('clicked')
+    }
+
     return (
-        <div className={classes.Card}>
-            <img src={props.country.flag.svgFile} alt={`${props.country.name}'s Flag`} className={classes[props.country.name]} />
-            <div className={classes.Content}>
+        <div className="Card" onFocus={_onFocus} onClick={_onClick} tabIndex="0">
+            <img src={props.country.flag.svgFile} alt={`${props.country.name}'s Flag`} className={props.country.name}/>
+            <div className="Content">
                 <p>{props.country.name}</p>
             </div>
         </div>
