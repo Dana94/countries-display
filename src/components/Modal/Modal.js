e23a6e179ae4440e057bf1877e953663ec03b363
Dana <dana.ottaviani@gmail.com>
2020-10-30 10:19:11 -0400

    fix: add ul labels

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 
 import './Modal.css';
 import { FilterContext } from '../../context/filter-context'
@@ -8,12 +8,28 @@ const Modal = props => {
 
     const filterContext = useContext(FilterContext);
 
-    console.log(filterContext.selectedCountry)
+    // console.log(filterContext.selectedCountry)
     return (
         <div className="modal">
             {/* <Cross /> */}
-            <img src={filterContext.selectedCountry?.flag.svgFile} alt={`${filterContext.selectedCountry?.name}'s Flag`}/>
+            <img
+                src={filterContext.selectedCountry?.flag.svgFile}
+                alt={`${filterContext.selectedCountry?.name}'s Flag`}
+                className="modal-flag"
+            />
             {filterContext.selectedCountry?.name}
+            <p id="languages">Languages:</p>
+            <ul aria-labelledby="languages">
+                {
+                    filterContext.selectedCountry?.officialLanguages.map(lang => <li key={lang.name}>{lang.name}</li>)
+                }
+            </ul>
+            <p id="currencies">Currencies:</p>
+            <ul aria-labelledby="currencies">
+                {
+                    filterContext.selectedCountry?.currencies.map(currency => <li key={currency.name}>{currency.name}</li>)
+                }
+            </ul>
         </div>
     )
 };
